7335f756dc69ec60476532043426f05d987e4bf2
Trowy <growety@gmail.com>
2012-12-29 17:07:44 +0700

    First init commit

---
+++ src/com/travianize/travianize/Travianize.java
@@ -0,0 +1,11 @@
+package com.travianize.travianize;
+
+public class Travianize {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello, World, from Travianize!");
+
+    }
+
+}
