d0527a07a572cb5f1196beecb401cf4c22f80794
Trowy <growety@gmail.com>
2013-01-18 14:16:35 +0800

    ver 0.0.6

--- src/com/travianize/travianize/Travianize.java
+++ src/com/travianize/travianize/Travianize.java
@@ -16,7 +16,7 @@ import java.util.logging.Level;
 
 public class Travianize extends Thread {
 
-    public static final String VERSION = "0.0.5";
+    public static final String VERSION = "0.0.6";
     public static final String APPLICATION_NAME = "Travianize";
     private static final String accountsInfoFileName = "accounts.conf";
     public static final String wrongCommand = "Wrong command";
